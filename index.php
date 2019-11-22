206b7bab16e677eaf3c6a2cdb2eb7bd860e3c5eb
alvin12345-cycber <57780639+alvin12345-cycber@users.noreply.github.com>
2019-11-22 10:47:45 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
      <marquee> <h1 style="font-family: couier;color red;">Alvin Romaguera</h1></marquee>
      <br><br><br><div style="border: 1px solid black;height: 500px; width: 300px;margin-left:500px;background-color:pink; border-radius: 20px; ">
      <center><h1 style="font-family: courier;"><?php echo "My Work ";?></h1>
-     <br><a href="welcome.php>button style="font-size: 20px;border-radius: 12px">Welcome</button></a>
-      <br><br><a href="getmethod.php>button style="font-size: 20px;border-radius: 12px">getmethod</button></a>
-      <br><br><a href="condition.php>button style="font-size: 20px;border-radius: 12px">condition</button></a>
-      <br><br><a href="displaymethod.php>button style="font-size: 20px;border-radius: 12px">displaymethod</button></a>    
+     <br><a href="welcome.php"><button style="font-size: 20px;border-radius: 12px">Welcome</button></a>
+     <br><br><a href="getmethod.php"><button style=" font-size: 20px; border-radius: 12px">getmethod</button></a>
+     <br><br><a href="condition.php"><button style=" font-size: 20px; border-radius: 12px">condition</button></a>
+     <br><br><a href="displaymethod.php"><button style=" font-size: 20px; border-radius: 12px ">displaymethod</button></a>    
+     <br><br><a href="loop.php"><button style=" font-size: 20px; border-radius: 12px ">loop</button></a>  
 
 </body>
 </html>
